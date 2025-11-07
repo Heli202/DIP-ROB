204022a7ac141f356696b60ad4eb168a9502812b
Heli202 <xxhelicopterdashxx@gmail.com>
2025-11-06 19:56:15 -0800

    feat: changing states to class, rename to arm

--- Robot Arm/main.py
+++ Robot Arm/main.py
@@ -1,11 +1,11 @@
 import xarm
 import serial as ser
-import states
+import arm
 
 if __name__ == "__main__":
     arm = xarm.Controller("USB")
-    states.set_state("home")
+    arm.set_state("home")
     #states.set_state("ready_to_grab")
     #states.set_state("ready_to_move")
     #states.set_state("lift")
-    print(states.get_joints())
+    print(arm.get_joints())
